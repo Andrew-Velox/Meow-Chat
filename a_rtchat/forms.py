f2780a6961fb89803087941139426566a1e6b6c5
Andrew-Velox <mohabbat.bd2020@gmail.com>
2025-10-01 14:35:21 +0600

    updating frontend

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -7,7 +7,7 @@ class ChatmessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ['body']
         widgets = {
-            'body': forms.TextInput(attrs={'placeholder': 'Add message ...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True}),
+            'body': forms.TextInput(attrs={'placeholder': 'Message #general', 'class': 'bg-gray-600 text-white placeholder-gray-400 border-0 rounded-lg px-4 py-3 focus:ring-2 focus:ring-indigo-500 focus:outline-none', 'maxlength': '300', 'autofocus': True}),
         }
 
 
@@ -16,8 +16,7 @@ class NewGroupForm(ModelForm):
         model = ChatGroup
         fields = ['groupchat_name']
         widgets = {
-            'groupchat_name': forms.TextInput(attrs={'placeholder': 'Add name ...', 'class': 'p-4 text-black', 'maxlength': '300', 'autofocus': True,
-            }),
+            'groupchat_name': forms.TextInput(attrs={'placeholder': 'Enter server name', 'class': 'bg-gray-700 text-white placeholder-gray-400 border border-gray-600 rounded px-3 py-2 focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none', 'maxlength': '100', 'autofocus': True}),
         }
 
 
@@ -27,7 +26,7 @@ class ChatRoomEditForm(ModelForm):
         model = ChatGroup
         fields = ['groupchat_name']
         widgets = {
-            'groupchat_name': forms.TextInput(attrs={'placeholder': 'Add name ...', 'class': 'p-4 text-xl font-bold mb-4', 'maxlength': '300', 'autofocus': True,
+            'groupchat_name': forms.TextInput(attrs={'placeholder': 'Enter server name', 'class': 'bg-gray-700 text-white placeholder-gray-400 border border-gray-600 rounded px-3 py-2 text-xl font-bold focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none', 'maxlength': '100', 'autofocus': True,
             }),
             
         }
